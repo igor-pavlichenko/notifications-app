6422903166e5a03149bd9841f3df54ded177dd79
Igor Pavlichenko <igor.pavlichenko@comon.pt>
2017-05-03 12:25:10 +0100

    Implement Header ocmponent
    Show input when search icon is clicked.

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,11 +2,36 @@ import React, { Component } from 'react';
 import './Header.css';
 
 class Header extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			inputIsActive: false,
+		}
+	}
+
+	toggleInputIsActive = () => {
+		let auxActive = this.state.inputIsActive;
+		this.setState({
+			inputIsActive: !auxActive,
+		});
+	}
+
 	render() {
 		return (
 			<div className="header">
+				<div className="fa fa-bars fa-lg fa-fw"></div>
 
+				<span className="title">Timeline</span>
+
+				<input type="text"
+						className={(this.state.inputIsActive ? "active" : "") + " searchInput"}
+						placeholder="Search..."/>
+
+				<div onClick={this.toggleInputIsActive} className="fa fa-search searchIcon fa-lg fa-fw"></div>
 			</div>
 		);
 	}
 }
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import '../bower_components/bootstrap/dist/css/bootstrap.min.css';
+import '../bower_components/components-font-awesome/css/font-awesome.min.css';
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+	<App />,
+	document.getElementById('root')
 );
